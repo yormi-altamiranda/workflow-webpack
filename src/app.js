8c770d43f78f28debac47dac3426a5d9c3ed347a
Yormi Altamiranda <yormian@gmail.com>
2019-04-23 18:34:35 -0500

    Source JS

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// require('./scss/styles.scss');
+import "jquery";
+import "./js/main";
+import "./scss/styles.scss";
+import "./static/img/logo-plantilla.svg";
+import "./static/img/webpack-logo.png";
+// alert('WorkFlow');
